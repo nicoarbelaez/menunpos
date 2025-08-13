d75ffb38a408e6bff1e8a810b919761551260b5c
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-08-13 16:03:44 -0500

    [web] feat: Se permite mas middelware

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,12 +1,22 @@
-import { type NextRequest } from 'next/server';
-import { updateSession } from '@/utils/supabase/middleware';
+import { NextResponse, type NextRequest } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const handlers = [
+    updateSession, // lógica de sesión
+  ];
+
+  for (const handler of handlers) {
+    const result = await handler(request);
+    if (result) {
+      return result; // si devuelve algo (redirect/response), se detiene aquí
+    }
+  }
+  return NextResponse.next(); // si ninguno respondió, continua normalmente
 }
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
