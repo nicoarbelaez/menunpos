ce186791528786665244a6ae42477dc6e4a99491
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-08-19 09:54:08 -0500

    [web] feat: Refactor de la funcion redirectToLogin y de los prefix

--- web/src/constans/url-prefix.ts
+++ web/src/constans/url-prefix.ts
@@ -1,6 +1,6 @@
 export const UrlPrefix = {
-  BUSINESS: "/c", // Para cuentas de negocio
-  CLIENT: "/t", // Para cuentas personales
+  BUSINESS: "t", // Para cuentas de negocio
+  CLIENT: "c", // Para cuentas personales
 } as const;
 
 export type UrlPrefix = typeof UrlPrefix[keyof typeof UrlPrefix];
