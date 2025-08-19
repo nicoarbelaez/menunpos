ea9879588a4ffcf189f705601a5995f227b75f57
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-08-19 09:07:37 -0500

    [web] feat: Agregamos el tipo del prefix

--- web/src/constans/url-prefix.ts
+++ web/src/constans/url-prefix.ts
@@ -2,3 +2,5 @@ export const UrlPrefix = {
   BUSINESS: "/c", // Para cuentas de negocio
   CLIENT: "/t", // Para cuentas personales
 } as const;
+
+export type UrlPrefix = typeof UrlPrefix[keyof typeof UrlPrefix];
